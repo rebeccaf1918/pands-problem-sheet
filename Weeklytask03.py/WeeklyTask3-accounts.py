7a3885191a0d94407b5f84ee8d0d4d8d51e9b017
Rebecca Feeley <G00425652@atu.ie>
2023-02-27 02:02:18 +0000

    Weekly task 3

---
+++ Weeklytask03.py/WeeklyTask3-accounts.py
@@ -0,0 +1,15 @@
+# accounts.py
+# reads in a 10 character account number and outputs the account 
+# number with only the last 4 digits showing (and the first 6 digits replaced with Xs).
+# Author: Rebecca Feeley
+
+
+account_number_to_input = str(input("Enter a 10 digit account number: "))
+
+#https://www.w3schools.com/python/ref_string_rjust.asp - I used this as inspiration for the code
+account_number = (account_number_to_input[6: ].rjust(len(account_number_to_input), "X"))
+
+print (account_number)
+
+
+
