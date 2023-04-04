f6fb3f2220fc5c36372760693e7de55fe63d4ef7
Rebecca Feeley <G00425652@atu.ie>
2023-04-04 23:45:31 +0100

    updated comments and filename

--- weekday.py
+++ weekday.py
@@ -1,23 +1,18 @@
 # Weekly Task 05
 # weekday.py
-# Program that 
+# Program that outputs whether or not today is a weekday.
 # Author: Rebecca Feeley
 
 from datetime import datetime
 
-dt = datetime.now() # get current datetime
-x = dt.isoweekday() # get current weekday as an integer value
+dt = datetime.now()
+Day_Name = (dt.strftime("%A"))
 
-if x > 5: # 1 = Monday; 2 = Tuesday; etc)
-    print ("It is the weekend, yay.")
+Weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+Weekend = ["Saturday", "Sunday"]
+
+if Day_Name in Weekday:
+    print ("Yes, unfortunately, it is a weekday.")
 
 else:
-    print ("Yes, unfortunately, today is a weekday.")
-
-
-# REFERENCES:
-# https://pynative.com/python-get-the-day-of-week
-
-
-
-
+    print ("Yay, it is the weekend")
